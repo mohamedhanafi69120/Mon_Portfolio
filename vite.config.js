51bc67fd550088e3430592ecd58f4c9271b6a665
Your Name <you@example.com>
2025-02-06 17:36:36 +0100

    mise a jout du CV

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Remplace "NOM_DU_REPO" par le nom exact de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: "/NOM_DU_REPO/",
+  base: "/Mon_Portfolio/", // Nom de ton repo GitHub ici
 });
