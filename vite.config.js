60137e39008c33c113ab8bfb53d9ba26a2d3cfb2
Your Name <you@example.com>
2025-02-06 13:00:30 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Remplace "NOM_DU_REPO" par le nom exact de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/NOM_DU_REPO/",
+});
