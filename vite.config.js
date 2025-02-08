4a934b8e217db22e807924498e48983c7ed79ae2
Your Name <you@example.com>
2025-02-08 12:01:50 +0100

    Déploiement du build optimisé avec correction de .gitignore

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Mon_Portfolio/", // Nom de ton repo GitHub ici
-});
